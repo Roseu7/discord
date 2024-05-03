95df37053f7702327aafaf2003ec2b28f4ae0202
c0a21105 <c0a2110575@edu.teu.ac.jp>
2024-05-03 18:40:11 +0900

    修正

--- modules/mcrconDef.py
+++ modules/mcrconDef.py
@@ -5,7 +5,7 @@ import dotenv
 dotenv.load_dotenv()
 SERVER_ADDRESS = os.getenv("SERVER_ADDRESS")
 SERVER_PASS = os.getenv("SERVER_PASS")
-SERVER_PORT = os.getenv("SERVER_PORT")
+SERVER_PORT = int(os.getenv("SERVER_PORT"))
 
 def mc_getlist():
     with MCRcon(SERVER_ADDRESS, SERVER_PASS, SERVER_PORT) as mcr:
